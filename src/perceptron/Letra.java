3ce8f2484a926e1dd3c4f3061c4cedff331b6e50
Marcos Avila Isidoro <marcavis@yahoo.com.br>
2017-05-25 02:48:15 -0300

    primeiro commit

---
+++ src/perceptron/Letra.java
@@ -0,0 +1,89 @@
+package perceptron;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Letra {
+	private String nome;
+	private int numero;
+	private char[] caracteres;
+	private int[] amostras;
+	//como é representado um resultado no perceptron; por ex., {1,0,0,0,0,0,0} é a letra A
+	private int[] saida;
+	
+	public Letra(String nome, int numero, int[] saida) {
+		setNome(nome);
+		setNumero(numero);
+		setSaida(saida);
+		try {
+			FileReader fr = new FileReader(new File("Letras/" + nome + "-" + numero + ".txt"));
+			BufferedReader br = new BufferedReader(fr);
+			String conteudo = "";
+			String linha;
+			while((linha=br.readLine())!=null) {
+				conteudo += linha;
+			}
+			caracteres = conteudo.toCharArray();
+			br.close();
+			fr.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		amostras = new int[caracteres.length];
+		for (int i = 0; i < caracteres.length; i++) {
+			if(caracteres[i] == '#') {
+				amostras[i] = 1;
+			} else if (caracteres[i] == '-') {
+				amostras[i] = -1;
+			} else {
+				amostras[i] = 0;
+			}
+			//System.out.println(caracteres[i]+"-"+amostras[i]);
+		}
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public char[] getCaracteres() {
+		return caracteres;
+	}
+	
+	public void setCaracteres(char[] caracteres) {
+		this.caracteres = caracteres;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int[] getSaida() {
+		return saida;
+	}
+
+	public void setSaida(int[] saida) {
+		this.saida = saida;
+	}
+
+	public int[] getAmostras() {
+		return amostras;
+	}
+
+	public void setAmostras(int[] amostras) {
+		this.amostras = amostras;
+	}
+	
+	
+	
+}
---
+++ src/perceptron/Main.java
@@ -0,0 +1,61 @@
+package perceptron;
+
+import java.text.DecimalFormat;
+
+public class Main {
+	
+	private static int tiposDeLetras = 7;
+	private static int variantesDeLetras = 3;
+	private static int qtLetras = tiposDeLetras * variantesDeLetras;
+	private static Letra[] letras = new Letra[tiposDeLetras * variantesDeLetras];
+	private static String[] ordemDeLetras = {"A", "B", "C", "D", "E", "J", "K"};
+	private static int[][] saida = 
+			{{1,0,0,0,0,0,0},
+			{0,1,0,0,0,0,0},
+			{0,0,1,0,0,0,0},
+			{0,0,0,1,0,0,0},
+			{0,0,0,0,1,0,0},
+			{0,0,0,0,0,1,0},
+			{0,0,0,0,0,0,1}};
+	private static int tamanhoLetra = 63;
+	private static double[] pesos = new double[tamanhoLetra];
+	private static double taxa = 0.1;
+
+	public static double[] inicializarPesos(double[] pesos) {
+		for (int i = 0; i < pesos.length; i++) {
+			pesos[i] = 0.0;
+		}
+		return pesos;
+	}
+	
+	public static void main(String[] args) {
+		//inicializando cada letra com seu caractere, versão do caractere e saída esperada (por exemplo, A é 1000000
+		for (int i = 0; i < qtLetras; i++) {
+			letras[i] = new Letra(ordemDeLetras[i/variantesDeLetras], (i/tiposDeLetras)+1, saida[i/variantesDeLetras]);
+		}
+		pesos = inicializarPesos(pesos);
+		for(int i = 0; i < qtLetras; i++) {
+			double[] z = new double[tamanhoLetra];
+			for(int j = 0; j < tamanhoLetra; j++) {
+				z[j] = letras[i].getAmostras()[j] * pesos[j];
+			}
+			System.out.println(mostraLinha(i+1, z, letras[i].getSaida()));
+		}
+	}
+
+	public static String mostraLinha(int linha, double[] valores, int[] saida) {
+		String resultado = String.format("%2s", linha);
+		resultado += " | ";
+		DecimalFormat format = new DecimalFormat("#.00");
+		for(double v: valores) {
+			resultado += String.format("%5s", format.format(v));
+			resultado += " ";
+		}
+		resultado += "|";
+		for(int i: saida) {
+			resultado += String.format("%2s", i);
+			resultado += " ";
+		}
+		return resultado;
+	}
+}
